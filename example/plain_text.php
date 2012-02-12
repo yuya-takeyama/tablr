c755510aef9db76b8eb54759cb1dae7aa5bae76c
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-02-12 17:27:53 +0900

    Fix: example.

--- example/plain_text.php
+++ example/plain_text.php
@@ -2,13 +2,18 @@
 set_include_path(dirname(__FILE__) . '/../src' . PATH_SEPARATOR . get_include_path());
 require_once 'Tablr.php';
 require_once 'Tablr/Formatter/PlainText.php';
+require_once 'Tablr/Aggregator/Average.php';
+require_once 'Tablr/Aggregator/Sum.php';
 
-$tablr = new Tablr;
-$table = $tablr->createTable(array(
-    array('year-month' => '2011/01', 'registered' =>  100, 'retired' => 10, 'total' =>   90),
-    array('year-month' => '2011/02', 'registered' =>  200, 'retired' => 40, 'total' =>  250),
-    array('year-month' => '2011/03', 'registered' =>  140, 'retired' => 90, 'total' =>  300),
-    array('year-month' => '2011/04', 'registered' => 1300, 'retired' => 15, 'total' => 1785),
+$table = new Tablr_Table(array(
+    array('year-month' => '2011/01', 'registered' =>  100, 'retired' =>  10, 'inc_dec' =>   90),
+    array('year-month' => '2011/02', 'registered' =>  200, 'retired' =>  40, 'inc_dec' =>  160),
+    array('year-month' => '2011/03', 'registered' =>  140, 'retired' => 190, 'inc_dec' =>  -50),
+    array('year-month' => '2011/04', 'registered' => 1300, 'retired' =>  15, 'inc_dec' => 1285),
 ));
-$table->setHeader(array('年月', '入会数', '退会数', '合計会員数'));
+$table->setHeader(array('Month', 'Registered', 'Retired', 'Inc/Dec'));
+
+$table->addAggregator(new Tablr_Aggregator_Average);
+$table->addAggregator(new Tablr_Aggregator_Sum);
+
 echo $table->format(new Tablr_Formatter_PlainText);
